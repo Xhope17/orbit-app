10947fd479a46b4e82fa7add59d7dad12f95901e
Mora <morabryan322@gmail.com>
2026-05-28 22:52:11 -0500

    fix: corregir rutas en deploy

--- frontend/orbit-app/src/app/app.config.ts
+++ frontend/orbit-app/src/app/app.config.ts
@@ -3,11 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    //provide para las rutas de despliegue de componentes
     provideHttpClient(),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    }
   ],
 };
