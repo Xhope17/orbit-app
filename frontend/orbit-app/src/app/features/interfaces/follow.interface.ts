921c1d567ce9f6bb0bfb4557d8b64b4190820e2e
Mora <morabryan322@gmail.com>
2026-06-01 13:38:59 -0500

    feat: implementar servicio e interface follow

---
+++ frontend/orbit-app/src/app/features/interfaces/follow.interface.ts
@@ -0,0 +1,6 @@
+export interface FollowInterface {
+  profileId: string;
+  username: string;
+  displayName: string;
+  avatarUrl: string | null;
+}
---
+++ frontend/orbit-app/src/app/features/services/follow.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { ApiResponse } from '../../shared/interfaces/apiResponse.interface';
+import { PaginatedResponse } from '../interfaces/post.interface';
+import { FollowInterface } from '../interfaces/follow.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FollowService {
+  private http = inject(HttpClient);
+  private readonly apiUrl = environment.apiUrl;
+
+  followUser(username: string) {
+    return this.http.post<ApiResponse<any>>(`${this.apiUrl}/profiles/${username}/follow`, {});
+  }
+
+  unfollowUser(username: string) {
+    return this.http.delete<ApiResponse<any>>(`${this.apiUrl}/profiles/${username}/follow`);
+  }
+
+  getFollowers(username: string, page: number = 1, pageSize: number = 20) {
+    return this.http.get<ApiResponse<PaginatedResponse<FollowInterface>>>(
+      `${this.apiUrl}/profiles/${username}/followers?page=${page}&pageSize=${pageSize}`,
+    );
+  }
+
+  getFollowing(username: string, page: number = 1, pageSize: number = 20) {
+    return this.http.get<ApiResponse<PaginatedResponse<FollowInterface>>>(
+      `${this.apiUrl}/profiles/${username}/following?page=${page}&pageSize=${pageSize}`,
+    );
+  }
+}
